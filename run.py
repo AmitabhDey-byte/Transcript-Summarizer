2bef4f11728f5f593ce7fea1d8aa9f351b35d608
AmitabhDey-Byte <amitabhdey101@gmail.com>
2026-04-05 12:07:45 +0530

    the pipeline is almost done..there is some package conflict but will fix it in a few hours

---
+++ run.py
@@ -0,0 +1,22 @@
+from langchain_community.document_loaders import YoutubeLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.llms import OpenAI
+from langchain.chains import LLMChain
+from langchain import PromptTemplate
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.vectorstores import FAISS
+from dotenv import load_dotenv
+
+load_dotenv()
+
+embeddings = OpenAIEmbeddings()
+
+def yt_load(video_url: str) -> FAISS:
+    loader = YoutubeLoader.from_youtube_url(video_url)
+    transcript = loader.load()
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    texts = text_splitter.split_documents(transcript)
+    vectorstore = FAISS.from_documents(texts, embeddings)
+    return vectorstore 
+
+print(yt_load("https://www.youtube.com/watch?v=lG7Uxts9SXs&t=751s"))
